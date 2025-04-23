49dedb5f03fce7549313bb786914d06b6d3a1f57
ON24niklas <nik.foerch.24@lehre.mosbach.dhbw.de>
2025-04-23 19:25:18 +0200

    misslungene Gruppenanzeige über renderGroups() eingebaut

---
+++ src/components/Groups.ts
@@ -0,0 +1,26 @@
+import { groups } from "/Users/niklas.fcb/wm-planer/src/data/matches.ts"; 
+
+export function renderGroups() {
+  const container = document.getElementById("groups-container");
+
+  if (!container) return;
+
+  groups.forEach((group, index) => {
+    const groupDiv = document.createElement("div");
+    groupDiv.className = "group";
+
+    const title = document.createElement("h2");
+    title.textContent = `Gruppe ${String.fromCharCode(65 + index)}`;
+    groupDiv.appendChild(title);
+
+    const list = document.createElement("ul");
+    group.teams.forEach((team) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = team;
+      list.appendChild(listItem);
+    });
+
+    groupDiv.appendChild(list);
+    container.appendChild(groupDiv);
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
-import { groups } from './data/matches';
-function showGroups() {
-    const container = document.getElementById('groups-container');
-  
-    if (!container) return;
-  
-    groups.forEach((group, index) => {
-      const groupDiv = document.createElement('div');
-      groupDiv.className = 'group';
-      
-      const title = document.createElement('h2');
-      title.textContent = `Gruppe ${String.fromCharCode(65 + index)}`;
-      groupDiv.appendChild(title);
-  
-      const list = document.createElement('ul');
-      group.teams.forEach((team) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = team;
-        list.appendChild(listItem);
-      });
-  
-      groupDiv.appendChild(list);
-      container.appendChild(groupDiv);
-    });
-  }
-  
-  showGroups();
-  
+import { renderGroups } from "./components/Groups";
+
+renderGroups();
